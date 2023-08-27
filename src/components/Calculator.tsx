6a53673aba093f338e9d882a1be2c0f8fb782a6d
claudia.tss <claudia.tss@outlook.com>
2023-08-26 21:18:20 -0300

    Calculadora colorida

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -3,9 +3,9 @@ import Screen from "./Screen";
 
 const Calculator = () => {
   return (
-    <div className="flex flex-col gap-16 bg-yellow-300 h-screen">
-      <h1 className="text-xl">Calculadora em React</h1>
-      <div className="h-1/2 w-96 bg-purple-300 border-2 border-black ml-40">
+    <div className="flex flex-col align-baseline gap-8 h-screen">
+      <h1 className="text-4xl">Calculadora em React</h1>
+      <div className="h-1/2 w-96 bg-gradient-to-t from-indigo-500 to indigo-300 border-2 border-black ml-80">
         <Screen children={0} />
         <div className="grid grid-flow-row grid-cols-4 grid-rows-4 gap-4 text-xl m-6">
           <Button value="1" />
